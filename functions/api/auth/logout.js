f2e6cae266b0dc86d71448587822c5bc9934fdf0
hiquedenardi <hiquedenardi@gmail.com>
2026-03-24 21:21:35 -0300

    Delete functions directory

--- functions/api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-import { json, getCookie, destroySession, clearCookie } from '../../_utils';
-
-export async function onRequestPost(context) {
-  const { request, env } = context;
-  const sessionId = getCookie(request, 'precificador_session');
-  if (sessionId) await destroySession(env, sessionId);
-  return json({ ok: true }, 200, { 'Set-Cookie': clearCookie('precificador_session') });
-}
